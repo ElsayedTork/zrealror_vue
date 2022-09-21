bfada2dbbf494cef5dbd596c1fce9bcbf76f8efa
ElsayedTork <elsayed.torky@inovola.com>
2022-09-21 10:27:00 +0200

    handle menue and advertise ment

--- src/component/profile/index.vue
+++ src/component/profile/index.vue
@@ -2,7 +2,49 @@
   <div class="profile">
     <div class="container">
       <div class="row">
-        <section class="col-sm-2"></section>
+        <section class="col-sm-2 profile__left">
+          <ul class="profile__left__list">
+            <li class="active">
+              <img
+                src="./../../assets/icons/profile/profile-circle.svg"
+                alt=""
+              />
+              <span>my profile</span>
+            </li>
+            <li>
+              <div class="dropdown">
+                <img src="./../../assets/icons/profile/wishlist.svg" alt="" />
+                <button
+                  class="btn px-0 pe-0 ps-1 dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <span>My Wishlists</span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <img
+                src="./../../assets/icons/profile/PaperDownload.svg"
+                alt=""
+              />
+              <span>My Requests</span>
+            </li>
+            <li>
+              <img src="./../../assets/icons/profile/logout.svg" alt="" />
+              <span>Logout</span>
+            </li>
+          </ul>
+          <div class="profile__left__advertisement"></div>
+        </section>
         <section class="col-sm-7 profile__center">
           <h3>My Profile</h3>
           <div class="profile__center__about">
