7fdce60507f24f7d42c20d1b5237cdc2a614927d
ElsayedTork <elsayed.torky@inovola.com>
2022-09-26 11:46:45 +0200

    start handle layout for properites

---
+++ src/pages/Properties/index.vue
@@ -0,0 +1,5 @@
+<template>Prop</template>
+<script>
+export default {};
+</script>
+<style></style>
--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from './../pages/home/index.vue';
 import Profile from './../pages/profile/index.vue';
 import Edit from './../pages/edit/index.vue';
-
+import Properties from './../pages/Properties/index.vue';
 const routes = [
   { path: '/', component: Home },
   {
@@ -12,6 +12,10 @@ const routes = [
   {
     path: '/profile',
     component: Profile,
+  },
+  {
+    path: '/prop',
+    component: Properties,
   },
 ];
 
