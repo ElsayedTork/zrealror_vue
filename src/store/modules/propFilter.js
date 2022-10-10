07d12a46ce01b3b2a01952b8be16ccae576cf385
ElsayedTork <elsayed.torky@inovola.com>
2022-10-10 08:35:16 +0200

    mark before handle and group search pagination

--- src/store/modules/propFilter.js
+++ src/store/modules/propFilter.js
@@ -148,8 +148,6 @@ export default {
   },
   mutations: {
     itemSearch(state, arr) {
-      //  let propArr = ['item.category', 'location', 'propType', 'floor'];
-      console.log(arr, 'Mutation');
       this.state.propInformations = state.propInformations.filter((item) => {
         switch (arr.length) {
           case 4:
@@ -175,141 +173,127 @@ export default {
         }
       });
     },
-    // categorySearch(state, category) {
-    //   console.log(category);
-    //   this.state.propInformations = state.propInformations.filter((item) => {
-    //     return item.category === category;
-    //   });
-    // },
-    // typeSearch(state, type) {
-    //   console.log(type);
-    //   this.state.propInformations = state.propInformations.filter((item) => {
-    //     return item.propType === type;
-    //   });
-    // },
     filterItems(state, index) {
-      state.propInformations = [
-        {
-          id: 0,
-          title: 'Renovated Apartment 1',
-          location: 'Tanat ,Egypt',
-          bed: 2,
-          baths: 1,
-          sqft: 2500,
-          wifi: false,
-          page: 1,
-        },
-        {
-          id: 1,
-          title: 'Amazing Apartment 2',
-          location: 'Alex,Egypt',
-          bed: 4,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 1,
-        },
-        {
-          id: 2,
-          title: 'Renovated Apartment 3',
-          location: 'october, Egypt',
-          bed: 5,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 1,
-        },
-        {
-          id: 3,
-          title: 'Renovated Apartment 4',
-          location: 'Tanat ,Egypt',
-          bed: 2,
-          baths: 1,
-          sqft: 2500,
-          wifi: false,
-          page: 2,
-        },
-        {
-          id: 4,
-          title: 'Amazing Apartment 5',
-          location: 'Alex,Egypt',
-          bed: 4,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 2,
-        },
-        {
-          id: 5,
-          title: 'Renovated Apartment 6',
-          location: 'october, Egypt',
-          bed: 5,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 2,
-        },
-        {
-          id: 6,
-          title: 'Renovated Apartment 7',
-          location: 'Tanat ,Egypt',
-          bed: 2,
-          baths: 1,
-          sqft: 2500,
-          wifi: false,
-          page: 3,
-        },
-        {
-          id: 7,
-          title: 'Amazing Apartment 8',
-          location: 'Alex,Egypt',
-          bed: 4,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 3,
-        },
-        {
-          id: 8,
-          title: 'Renovated Apartment 9',
-          location: 'october, Egypt',
-          bed: 5,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 3,
-        },
-        {
-          id: 9,
-          title: 'Renovated Apartment 10',
-          location: 'october, Egypt',
-          bed: 5,
-          baths: 2,
-          sqft: 2500,
-          wifi: true,
-          page: 4,
-        },
-      ];
+      // state.propInformations = [
+      //   {
+      //     id: 0,
+      //     title: 'Renovated Apartment 1',
+      //     location: 'Tanat ,Egypt',
+      //     bed: 2,
+      //     baths: 1,
+      //     sqft: 2500,
+      //     wifi: false,
+      //     page: 1,
+      //   },
+      //   {
+      //     id: 1,
+      //     title: 'Amazing Apartment 2',
+      //     location: 'Alex,Egypt',
+      //     bed: 4,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 1,
+      //   },
+      //   {
+      //     id: 2,
+      //     title: 'Renovated Apartment 3',
+      //     location: 'october, Egypt',
+      //     bed: 5,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 1,
+      //   },
+      //   {
+      //     id: 3,
+      //     title: 'Renovated Apartment 4',
+      //     location: 'Tanat ,Egypt',
+      //     bed: 2,
+      //     baths: 1,
+      //     sqft: 2500,
+      //     wifi: false,
+      //     page: 2,
+      //   },
+      //   {
+      //     id: 4,
+      //     title: 'Amazing Apartment 5',
+      //     location: 'Alex,Egypt',
+      //     bed: 4,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 2,
+      //   },
+      //   {
+      //     id: 5,
+      //     title: 'Renovated Apartment 6',
+      //     location: 'october, Egypt',
+      //     bed: 5,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 2,
+      //   },
+      //   {
+      //     id: 6,
+      //     title: 'Renovated Apartment 7',
+      //     location: 'Tanat ,Egypt',
+      //     bed: 2,
+      //     baths: 1,
+      //     sqft: 2500,
+      //     wifi: false,
+      //     page: 3,
+      //   },
+      //   {
+      //     id: 7,
+      //     title: 'Amazing Apartment 8',
+      //     location: 'Alex,Egypt',
+      //     bed: 4,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 3,
+      //   },
+      //   {
+      //     id: 8,
+      //     title: 'Renovated Apartment 9',
+      //     location: 'october, Egypt',
+      //     bed: 5,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 3,
+      //   },
+      //   {
+      //     id: 9,
+      //     title: 'Renovated Apartment 10',
+      //     location: 'october, Egypt',
+      //     bed: 5,
+      //     baths: 2,
+      //     sqft: 2500,
+      //     wifi: true,
+      //     page: 4,
+      //   },
+      // ];
       this.state.propInformations = state.propInformations.filter(
         (item) => item.page === index
       );
     },
     priceFilter(state, selectedPrice) {
-      const byMatss = state.propInformations.sort((a, b) => {
+      state.propInformations = this.state.propInformations.sort((a, b) => {
         if (selectedPrice == 'Lowest Price') {
           return a.price - b.price;
         } else {
           return b.price - a.price;
         }
       });
-      this.state.propInformations = byMatss;
     },
   },
   actions: {
     itemSearch({ commit }, arr) {
       commit('itemSearch', arr);
     },
-
     priceFilter({ commit }, selectedPrice) {
       commit('priceFilter', selectedPrice);
     },
